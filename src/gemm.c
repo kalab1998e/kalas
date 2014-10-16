1ff3491a1114365df8535c9d70ebc66e9a909c69
Kiyotaka ATSUMI <kiyotaka@ka-lab.jp>
2014-10-16 17:51:02 +0900

    writing first code

--- src/gemm.c
+++ src/gemm.c
@@ -1,10 +1,26 @@
 #include "kalasState.h"
 #include "kalas.h"
 
-void kalasGemm( kalasState *state, typeKind *type, char *ta, char *tb, int *M, int *N, int *K, double *alpha, void *a, int *lda, void *b, int *ldb, double *beta, void *c, int *ldc) {
-  
+static const clblasOrder order = clblasRowMajor;
+
+clblasTranspose transChgF2C( char t) {
+  switch ( t) {
+  case 'N':
+    return clblasNoTrans;
+  case 'T':
+    return clblasTrans;
+  }
+  return clblasNoTrans;
 }
 
-void kalasGemm_( kalasState *state, typeKind type, char ta, char tb, int M, int N, int K, double alpha, void *a, int lda, void *b, int ldb, double beta, void *c, int ldc) {
+void kalasGemm( kalasState *state, typeKind type, char ta, char tb, int M, int N, int K, double alpha, void *a, int lda, void *b, int ldb, double beta, void *c, int ldc) {
+  clblastTranspose transA, transB;
+  cl_int err;
+  cl_mem bufA, bufB, bufC;
+  cl_event event = NULL;
+  int ret = 0;
+  
+  transA = transChgF2C( ta);
+  transB = transChgF2C( tb);
   
 }
