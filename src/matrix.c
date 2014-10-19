6d48a5aaf970b6a0d3a8938cf16e4a1e4cac51ce
Kiyotaka ATSUMI <kiyotaka@ka-lab.jp>
2014-10-19 18:01:02 +0900

    compare between openblas and clBLAS

--- src/matrix.c
+++ src/matrix.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <stdbool.h>
 #include <time.h>
-#include <acml.h>
 #include "matrix.h"
 #include "kadbg.h"
 
--- src/sample.c
+++ src/sample.c
@@ -1,4 +1,5 @@
 #include <sys/time.h>
+#include <f77blas.h>
 #include "matrix.h"
 #include "kalas.h"
 #include "kalasState.h"
@@ -9,12 +10,14 @@ int main(void)
   KalasState *state;
   Matrix *a, *b, *c;
   int am, an, bm, bn, cm, cn;
+  float alpha = 1.0, beta = 0.0;
+  char ta = 'N', tb = 'N';
   struct timeval t;
   double ts, te;
   
   state = kalasStateNew();
   
-  for ( int i = 128; i <= 5792; i+=128) {
+  for ( int i = 128; i <= 4096; i+=4) {
     am = an = bm = bn = cm = cn = i;
     a = matrixNew( am, an, KALAS_FLOAT, 1);
     b = matrixNew( bm, bn, KALAS_FLOAT, 1);
@@ -22,17 +25,29 @@ int main(void)
 
     gettimeofday( &t, NULL);
     ts = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
+    sgemm_( &ta, &tb, &cm, &cn, &an, &alpha, a->elm, &an, b->elm, &bn, &beta,
+                c->elm, &cn);
+    gettimeofday( &t, NULL);
+    te = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
+    printf( "size: %d, time: %f s, speed: %f GFlops \t", am, (te - ts),
+            (double)am * (double)am * (double)am * 2.0
+            / ( te - ts) / 1000000000.0);
+
+    gettimeofday( &t, NULL);
+    ts = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
     kalasSgemm( state, 'N', 'N', cm, cn, an, 1.0, a->elm, an, b->elm, bn, 0.0,
                 c->elm, cn);
     gettimeofday( &t, NULL);
     te = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
-    printf( "size: %d, time: %f s, speed: %e GFlops\n", am, (te - ts),
+    printf( "size: %d, time: %f s, speed: %f GFlops\n", am, (te - ts),
             (double)am * (double)am * (double)am * 2.0
             / ( te - ts) / 1000000000.0);
 
     matrixDelete(a);
     matrixDelete(b);
     matrixDelete(c);
+
+    
   }
   kalasStateDelete( state);
 }
