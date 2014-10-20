1e7c400be9e98a13b1bb752769a3377b17eba5e0
Kiyotaka ATSUMI <kiyotaka@ka-lab.jp>
2014-10-20 17:54:00 +0900

    sample.c is able to run both sgemm and dgemm

--- src/sample.c
+++ src/sample.c
@@ -9,8 +9,10 @@ int main(void)
 {
   KalasState *state;
   Matrix *a, *b, *c;
+  typeKind type = KALAS_DOUBLE;
   int am, an, bm, bn, cm, cn;
-  float alpha = 1.0, beta = 0.0;
+  float falpha = 1.0, fbeta = 0.0;
+  double dalpha = 1.0, dbeta = 0.0;
   char ta = 'N', tb = 'N';
   struct timeval t;
   double ts, te;
@@ -19,14 +21,24 @@ int main(void)
  
   for ( int i = 128; i <= 4096; i+=4) {
     am = an = bm = bn = cm = cn = i;
-    a = matrixNew( am, an, KALAS_FLOAT, 1);
-    b = matrixNew( bm, bn, KALAS_FLOAT, 1);
-    c = matrixNew( cm, cn, KALAS_FLOAT, 0);
+    a = matrixNew( am, an, type, 1);
+    b = matrixNew( bm, bn, type, 1);
+    c = matrixNew( cm, cn, type, 0);
 
     gettimeofday( &t, NULL);
     ts = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
-    sgemm_( &ta, &tb, &cm, &cn, &an, &alpha, a->elm, &an, b->elm, &bn, &beta,
-                c->elm, &cn);
+    switch (type) {
+    case KALAS_FLOAT:
+      sgemm_( &ta, &tb, &cm, &cn, &an, &falpha, a->elm, &an,
+              b->elm, &bn, &fbeta,
+              c->elm, &cn);
+      break;
+    case KALAS_DOUBLE:
+      dgemm_( &ta, &tb, &cm, &cn, &an, &dalpha, a->elm, &an,
+              b->elm, &bn, &dbeta,
+              c->elm, &cn);
+      break;
+    }
     gettimeofday( &t, NULL);
     te = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
     printf( "size: %d, time: %f s, speed: %f GFlops \t", am, (te - ts),
@@ -35,8 +47,16 @@ int main(void)
 
     gettimeofday( &t, NULL);
     ts = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
-    kalasSgemm( state, 'N', 'N', cm, cn, an, 1.0, a->elm, an, b->elm, bn, 0.0,
-                c->elm, cn);
+    switch (type) {
+    case KALAS_FLOAT:
+      kalasSgemm( state, 'N', 'N', cm, cn, an, 1.0, a->elm, an, b->elm, bn, 0.0,
+                  c->elm, cn);
+      break;
+    case KALAS_DOUBLE:
+      kalasDgemm( state, 'N', 'N', cm, cn, an, 1.0, a->elm, an, b->elm, bn, 0.0,
+                  c->elm, cn);
+      break;
+    }
     gettimeofday( &t, NULL);
     te = t.tv_sec + (double)(t.tv_usec) / 1000000.0;
     printf( "size: %d, time: %f s, speed: %f GFlops\n", am, (te - ts),
